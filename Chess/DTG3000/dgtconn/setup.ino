c60b5186267c2189bc498f0d7df5c843b82d45f3
mahan518 <63216090+mahan518@users.noreply.github.com>
2022-09-18 19:22:46 -0400

    Delete Chess/DTG3000 directory

--- Chess/DTG3000/dgtconn/setup.ino
+++
@@ -1,8 +0,0 @@
-void setup()
-{
-  myWire.begin(); // join i2c bus (address optional for master)
-
-  lcd.begin(16, 2);              // start the library
-  lcd.setCursor(0, 0);
-  lcd.print("Push the buttons"); // print a simple message
-}
